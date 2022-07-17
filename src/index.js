9e23ca9e6250ed20b49289f8c3bc6db84b8a15de
sherpaaa <kosuke1201@gmail.com>
2022-07-17 22:29:37 +0900

    Routesの中の記述に変更

--- src/index.js
+++ src/index.js
@@ -18,12 +18,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <ScrollToTop />
-    <Routes>
-      <Route path={homeUrl + '/'} element={<Home />} />
-      <Route path={homeUrl + '/home'} element={<Home />} />
-      <Route path={homeUrl + '/resume'} element={<Resume />} />
-      <Route path={homeUrl + '/projects'} element={<Projects />} />
-      <Route path={homeUrl + '/contact'} element={<Contact />} />
+    <Routes basename={process.env.PUBLIC_URL}>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
   </BrowserRouter>,
